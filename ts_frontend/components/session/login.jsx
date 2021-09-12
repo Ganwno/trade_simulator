61ef59b65139d866fea595e172398e408bbb38ae
hannahliu-github <shhan.liu@gmail.com>
2021-09-12 17:44:46 -0400

    Rewrote user auth forms with react-bootstrap components and added validation with error messages.

--- ts_frontend/components/session/login.jsx
+++ ts_frontend/components/session/login.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
-import store from '../../store/store';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
 
 class Login extends React.Component {
     constructor(props) {
@@ -9,10 +14,18 @@ class Login extends React.Component {
         this.state = {
             username: '',
             password: '',
-            successfulLogin: false
+            // login form variables
+            successfulLogin: false,
+            validUsername: true,
+            checkedUsername: false,
+            usernameErrorMsg: '',
+            validPassword: true,
+            checkedPassword: false,
+            passwordErrorMsg: ''
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.validateForm = this.validateForm.bind(this);
     }
 
 
@@ -23,66 +36,147 @@ class Login extends React.Component {
     }
 
 
+    validateForm() {
+        // check if entered username or password is blank
+        const validUsername = this.state.username.length > 0;
+        const validPassword = this.state.password.length > 0;
+        let usernameErrorMsg = '';
+        let passwordErrorMsg = '';
+
+        if (!validUsername) {
+            usernameErrorMsg = 'Username cannot be blank';
+        }
+        if (!validPassword) {
+            passwordErrorMsg = 'Password cannot be blank';
+        }
+
+        this.setState({
+            validUsername: validUsername,
+            checkedUsername: true,
+            usernameErrorMsg: usernameErrorMsg,
+            validPassword: validPassword,
+            checkedPassword: true,
+            passwordErrorMsg: passwordErrorMsg
+        });
+
+        return validUsername && validPassword;
+    }
+
+
     handleSubmit(e) {
         // override default action from the form
-        e.preventDefault;
+        e.preventDefault();
 
-        this.props.loginUser({  
-            username: this.state.username,
-            password: this.state.password 
-            }
-        ).then( () => {
-            const errors = this.props.errors;
-            console.log(errors);
+        const validFormInputs = this.validateForm();
+
+        if (!validFormInputs){
+            e.stopPropagation();
+        }
+        else {
+            // send request to log in
+            this.props.loginUser({  
+                username: this.state.username,
+                password: this.state.password 
+                }
+            ).then( () => {
+                // collect error messages
+                const errors = this.props.errors;
+                console.log(errors);
                 
-            const successfulLogin = Object.keys(errors).length == 0;
-            if (successfulLogin){
-                // redirect to page after login
-                this.setState({ successfulLogin: true });
-            }
-            else {
-                // handle errors
-                Object.keys(errors).forEach(k => errors[k].forEach(v => console.log(k + " " + v)));
-            }
+                const successfulLogin = Object.keys(errors).length == 0;
+                if (successfulLogin){
+                    // record successful login and return
+                    this.setState({ 
+                        successfulLogin: true
+                    });
+                }
+                else {
+                    // handle errorss
+                    Object.keys(errors).forEach(k => {
+                        switch (k) {
 
-            }
-            );
+                            case "username":
+                                let usernameErrorMsg = 'Username ' + errors['username'][0];
+                                for (let i = 1; i<errors['username'].length; ++i) {
+                                    usernameErrorMsg.concat(', ' + errors['username'][i]);
+                                }
+                                this.setState({ usernameErrorMsg: usernameErrorMsg, validUsername: false });
+                                break;
+
+                            case "password":
+                                let passwordErrorMsg = 'Password ' + errors['password'][0];
+                                for (let i = 1; i < errors['password'].length; ++i) {
+                                    passwordErrorMsg.concat(', ' + errors['password'][i]);
+                                }
+                                this.setState({ passwordErrorMsg: passwordErrorMsg, validPassword: false });
+                                break;
+                        }
+                    });
+                }
+            });
+        }
     }
 
 
     render() {
 
+        // Redirect to hompage after successful login
         if (this.state.successfulLogin){
             return <Redirect to="/homepage" />
         }
 
         return (
-            <div className="login-form">
+            <Container 
+                className="login-form"
+            >
                 <h2>Log In</h2>
-                <form>
-
-                    <label>Username:
-                        <input
+                <Form 
+                    noValidate
+                    onSubmit={this.handleSubmit}
+                >
+                <Row>
+                    <Form.Group
+                        as={Col}
+                        md="3"
+                        controlId="validationUsername"
+                    >
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control
                             type="text"
+                            isInvalid={!this.state.validUsername}
+                            isValid={this.state.checkedUsername && this.state.validUsername}
                             value={this.state.username}
                             onChange={this.handleInput('username')}
                         />
-                    </label>
-                    <br></br>
-                    <label>Password:
-                        <input
+                        <Form.Control.Feedback 
+                            type="invalid"
+                        >{this.state.usernameErrorMsg}</Form.Control.Feedback>
+                    </Form.Group>
+                </Row>
+                <Row>
+                    <Form.Group
+                        as={Col}
+                        md="3"
+                        controlId="validationPassword"
+                    >
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control
                             type="password"
+                            isInvalid={!this.state.validPassword}
+                            isValid={this.state.checkedPassword && this.state.validPassword}
                             value={this.state.password}
                             onChange={this.handleInput('password')}
                         />
-                    </label>
-                    <br></br>
-                    <button
-                        onClick={this.handleSubmit}
-                    >Log in</button>
-
-                </form>
-            </div>
+                        <Form.Control.Feedback
+                            type="invalid"
+                        >{this.state.passwordErrorMsg}</Form.Control.Feedback>
+                    </Form.Group>
+                </Row>
+                <Button
+                    type="submit"
+                >Log in</Button>
+                </Form>
+            </Container>
         );
     }
 }
--- ts_frontend/components/session/signup.jsx
+++ ts_frontend/components/session/signup.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+
 class Signup extends React.Component {
 
     constructor(props){
@@ -8,10 +15,22 @@ class Signup extends React.Component {
         this.state = {
             username: '',
             password: '',
-            successfulLogin: false
+            reenterPassword: '',
+            // login form variables
+            successfulLogin: false,
+            validUsername: true,
+            checkedUsername: false,
+            usernameErrorMsg: '',
+            validPassword: true,
+            checkedPassword: false,
+            passwordErrorMsg: '',
+            validReenterPassword: true,
+            reenterPasswordErrorMsg: '',
+            checkedReenterPassword: false
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.validateForm = this.validateForm.bind(this);
     }
 
 
@@ -21,67 +40,176 @@ class Signup extends React.Component {
         };
     }
 
+    validateForm() {
+        // check if entered username or password is blank
+        // check if re-entered password matched password
+        const validUsername = this.state.username.length > 0;
+        const validPassword = this.state.password.length >= 6;
+        const validReenterPassword = this.state.password === this.state.reenterPassword;
+        let usernameErrorMsg = '';
+        let passwordErrorMsg = '';
+        let reenterPasswordErrorMsg = '';
+
+        if (!validUsername) {
+            usernameErrorMsg = 'Username cannot be blank';
+        }
+        if (!validPassword) {
+            passwordErrorMsg = 'Password is too short (minimum is 6 characters)';
+        }
+        if (!validReenterPassword) {
+            reenterPasswordErrorMsg = 'Passwords do not match';
+        }
+
+        this.setState({
+            validUsername: validUsername,
+            checkedUsername: true,
+            usernameErrorMsg: usernameErrorMsg,
+            validPassword: validPassword,
+            checkedPassword: true,
+            passwordErrorMsg: passwordErrorMsg,
+            validReenterPassword: validReenterPassword,
+            checkedReenterPassword: true,
+            reenterPasswordErrorMsg: reenterPasswordErrorMsg
+        });
+
+        return validUsername && validPassword && validReenterPassword;
+    }
+
 
     handleSubmit(e) {
         // override default action from the form
-        e.preventDefault;
+        e.preventDefault();
 
-        this.props.createNewUser({
-            username: this.state.username,
-            password: this.state.password
-            }
-        ).then( () => {
-            const errors = this.props.errors;
-            console.log(errors);
+        const validFormInputs = this.validateForm();
 
-            const successfulLogin = Object.keys(errors).length == 0;
-            if (successfulLogin) {
-                // redirect to page after login
-                this.setState({ successfulLogin: true });
+        if (!validFormInputs) {
+            e.stopPropagation();
+        }
+        else {
+            // send request to create account
+            this.props.createNewUser({
+                username: this.state.username,
+                password: this.state.password
             }
-            else {
-                // handle errors
-                Object.keys(errors).forEach(k => errors[k].forEach(v => console.log(k + " " + v)));
-            }
+            ).then(() => {
+                // collect error messages
+                const errors = this.props.errors;
+                console.log(errors);
 
-            }
-            );
+                const successfulLogin = Object.keys(errors).length == 0;
+                if (successfulLogin) {
+                    // record successful account creation and return
+                    this.setState({ successfulLogin: true });
+                }
+                else {
+                    // handle errorss
+                    Object.keys(errors).forEach(k => {
+                        switch (k) {
+
+                            case "username":
+                                let usernameErrorMsg = 'Username ' + errors['username'][0];
+                                for (let i = 1; i < errors['username'].length; ++i) {
+                                    usernameErrorMsg.concat(', ' + errors['username'][i]);
+                                }
+                                this.setState({ usernameErrorMsg: usernameErrorMsg, validUsername: false });
+                                break;
+
+                            case "password":
+                                let passwordErrorMsg = 'Password ' + errors['password'][0];
+                                for (let i = 1; i < errors['password'].length; ++i) {
+                                    passwordErrorMsg.concat(', ' + errors['password'][i]);
+                                }
+                                this.setState({ passwordErrorMsg: passwordErrorMsg, validPassword: false });
+                                break;
+                        }
+                    });
+                }
+
+            });
+        }
+
+
     }
 
 
     render() {
         
+        // Redirect to homepage after successful account creation
         if (this.state.successfulLogin){
             return <Redirect to="/homepage" />
         }
 
         return (
-            <div className="signup-form">
+            <Container 
+                className="signup-form"
+            >
                 <h2>Sign Up</h2>
-                <form>
-                    
-                    <label>Username:
-                        <input 
-                        type="text" 
-                        value={this.state.username}
-                        onChange={this.handleInput('username')}
-                        />
-                    </label>
-                    <br></br>
-                    <label>Password:
-                        <input
-                            type="password"
-                            value={this.state.password}
-                            onChange={this.handleInput('password')}
-                        />
-                    </label>
-                    <br></br>
-                    <button 
-                    onClick={this.handleSubmit}
-                    >Sign up!</button>
-
-                </form>
-            </div>
+                <Form
+                    noValidate
+                    onSubmit={this.handleSubmit}
+                >
+                    <Row>
+                        <Form.Group
+                            as={Col}
+                            md="3"
+                            controlId="validationUsername"
+                        >
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                isInvalid={!this.state.validUsername}
+                                isValid={this.state.checkedUsername && this.state.validUsername}
+                                value={this.state.username}
+                                onChange={this.handleInput('username')}
+                            />
+                            <Form.Control.Feedback
+                                type="invalid"
+                            >{this.state.usernameErrorMsg}</Form.Control.Feedback>
+                        </Form.Group>
+                    </Row>
+                    <Row>
+                        <Form.Group
+                            as={Col}
+                            md="3"
+                            controlId="validationPassword"
+                        >
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control
+                                type="password"
+                                isInvalid={!this.state.validPassword}
+                                isValid={this.state.checkedPassword && this.state.validPassword}
+                                value={this.state.password}
+                                onChange={this.handleInput('password')}
+                            />
+                            <Form.Control.Feedback
+                                type="invalid"
+                            >{this.state.passwordErrorMsg}</Form.Control.Feedback>
+                        </Form.Group>
+                    </Row>
+                    <Row>
+                        <Form.Group
+                            as={Col}
+                            md="3"
+                            controlId="validationReenterPassword"
+                        >
+                            <Form.Label>Re-enter Password:</Form.Label>
+                            <Form.Control
+                                type="password"
+                                isInvalid={!this.state.validReenterPassword}
+                                isValid={this.state.checkedReenterPassword && this.state.validReenterPassword}
+                                value={this.state.reenterPassword}
+                                onChange={this.handleInput('reenterPassword')}
+                            />
+                            <Form.Control.Feedback
+                                type="invalid"
+                            >{this.state.reenterPasswordErrorMsg}</Form.Control.Feedback>
+                        </Form.Group>
+                    </Row>
+                    <Button
+                        type="submit"
+                    >Log in</Button>
+                </Form>
+            </Container>
         );
     }
 };
