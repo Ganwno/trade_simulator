55eca26c72cd6d032928c0e22b1824f9bede0799
hannahliu-github <shhan.liu@gmail.com>
2021-09-12 17:02:50 -0400

    Add CSS style loader and react-bootstrap styling.

--- ts_frontend/components/root.jsx
+++ ts_frontend/components/root.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './app';
 
 export default ({ store }) => (
