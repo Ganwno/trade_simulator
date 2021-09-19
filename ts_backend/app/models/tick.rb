5bc7ed70558c2708f4ee92f625cb422c57201404
hannahliu-github <shhan.liu@gmail.com>
2021-09-19 17:13:46 -0400

    Tick model implementation and tick data API helpers.

--- ts_backend/app/models/tick.rb
+++ ts_backend/app/models/tick.rb
@@ -1,2 +1,30 @@
 class Tick < ApplicationRecord
+@@quote_sep = '_'
+
+validates :simulation_id, :timestamp, :quote_set, presence: true
+
+# public class methods
+
+def self.new_from_quote_array(simulation_id, timestamp, quote_array)
+    quote_set = (quote_array.map { |quote| quote.to_s}).join(@@quote_sep)
+    Tick.new({simulation_id: simulation_id, timestamp: timestamp, quote_set: quote_set})
+end
+
+
+def self.find_by_timestamp(simulation_id, timestamp)
+    Tick.find_by(simulation_id: simulation_id, timestamp: timestamp)
+end
+
+
+# public instance methods
+
+def map_tickers_to_quotes(ticker_array)
+    quotes = self.quote_set.split(@@quote_sep).map { |quote_str| quote_str.to_f }
+    quote_dict = Hash.new()
+    (0...ticker_array.length).each do |i|
+        quote_dict[ticker_array[i]] = quotes[i]
+    end
+    quote_dict
+end
+
 end
---
+++ ts_backend/app/models/tickdata.rb
@@ -0,0 +1,164 @@
+require 'finnhub_ruby'
+require 'distribution'
+
+class TickData
+    @@finnhub_api_key = 'c50k1baad3ic9bdla5u0'
+    @@normal_generator = Distribution::Normal.rng(0, 1, 0)
+
+    attr_reader :tickers, :start_time, :end_time, :tick_data
+
+
+    def initialize(tickers, start_time, end_time)
+
+        # set API key
+        FinnhubRuby.configure do |config|
+            config.api_key['api_key'] = @@finnhub_api_key
+        end
+        @@finnhub_client = FinnhubRuby::DefaultApi.new
+
+        # get data from API
+        raw_tick_data = TickData.get_multiple_stock_candles(tickers, start_time, end_time)
+
+        @tickers = raw_tick_data.keys
+
+        if @tickers.length == 0
+            raise StandardError.new 'No data returned by API for tickers ' + tickers.join(', ') + 
+                ' between ' + start_time.to_s + ' and ' + end_time.to_s + '.' 
+        end
+
+        min_start_time  = raw_tick_data[@tickers[0]]['start_time']
+        max_end_time    = raw_tick_data[@tickers[0]]['end_time']
+
+        @tickers.each do |ticker|
+            min_start_time  = [raw_tick_data[ticker]['start_time'], min_start_time].min
+            max_end_time    = [raw_tick_data[ticker]['end_time'], max_end_time].max
+        end
+
+        @start_time = min_start_time
+        @end_time = max_end_time + 60
+
+        # format as array of ticks
+        tick_data = Hash.new()
+        @tickers.each do |ticker|
+            tick_data = generate_ticks(tick_data, raw_tick_data[ticker], @start_time, @end_time)
+        end
+        @tick_data = tick_data
+    end
+
+
+    # public class methods
+
+    def self.get_stock_candles(ticker, start_time, end_time)
+
+        res = @@finnhub_client.stock_candles(ticker, '1', start_time, end_time).to_hash
+
+        if res[:s] == 'no_data'
+            # handle no data
+            raise StandardError.new 'No data returned by API for for ticker ' + ticker +
+                ' between ' + start_time.to_s + ' and ' + end_time.to_s + '.' 
+        else
+            data = Hash.new()
+            data['start_time'] = res[:t][0]
+            data['end_time'] = res[:t][-1]
+
+            (0...res[:t].length).each do |i|
+                data[res[:t][i]] = {o: res[:o][i],
+                                    h: res[:h][i],
+                                    l: res[:l][i],
+                                    c: res[:c][i]
+                                }
+            end
+            data
+        end
+    end
+
+
+    def self.get_multiple_stock_candles(tickers, start_time, end_time)
+        res = Hash.new()
+        tickers.each do |ticker|
+            begin
+                res[ticker] = get_stock_candles(ticker, start_time, end_time)
+            rescue StandardError => e
+                puts(e.message)
+            end
+        end
+        res
+    end
+
+
+    # public instance methods
+
+    def generate_ticks(generated_ticks, raw_tick_data, start_time, end_time)
+        # get number of ticks
+        one_over_60 = 1 / 60.0
+        num_minutes = (end_time - start_time) / 60
+
+        minute_start = start_time
+        last_quote = raw_tick_data[raw_tick_data['start_time']][:o]
+
+        (0...num_minutes).each do |i|
+            generate = false
+
+            if raw_tick_data[minute_start]
+                # generate ticks for this name and add to quotes array
+                generate = true
+
+                open_i  = raw_tick_data[minute_start][:o]
+                high_i  = raw_tick_data[minute_start][:h]
+                low_i   = raw_tick_data[minute_start][:l]
+                close_i = raw_tick_data[minute_start][:c]
+
+                z = 60.times.map { @@normal_generator.call }
+                sum_zn = z.inject { |x, s| s += x }
+                running_sum_zn = 0
+
+                # standard deviation in this minute
+                sigma = high_i - low_i
+                sigma_over_60 = sigma * one_over_60
+                mean_correction = sigma_over_60 * one_over_60 * sum_zn
+            end
+
+            # generate next minute
+            (0...60).each do |n|
+                timestamp = minute_start + n
+
+                if generate
+                    
+                    quote = sigma_over_60 * running_sum_zn - 
+                        n * mean_correction + 
+                        n * one_over_60 * close_i + 
+                        (60 - n) * one_over_60 * open_i
+
+                    running_sum_zn += z[n]
+
+                    quote = [low_i, [high_i, quote].min].max
+                    last_quote = quote
+                else
+                    quote = last_quote
+                end
+                
+                if generated_ticks[timestamp] && generated_ticks[timestamp].kind_of?(Array)
+                    generated_ticks[timestamp].append(quote)
+                else
+                    generated_ticks[timestamp] = [quote]
+                end
+            end
+
+            # move ahead 1 minute
+            minute_start += 60
+        end
+        
+        # append last close
+        timestamp = end_time
+        quote = raw_tick_data[raw_tick_data['end_time']][:c]
+
+        if generated_ticks[timestamp] && generated_ticks[timestamp].kind_of?(Array)
+            generated_ticks[timestamp].append(quote)
+        else
+            generated_ticks[timestamp] = [quote]
+        end
+
+        generated_ticks
+    end
+
+end
