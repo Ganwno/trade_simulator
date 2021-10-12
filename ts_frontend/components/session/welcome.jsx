4a3ebaa07e286eda77e510222bfe00a0df9b456c
hannahliu-github <shhan.liu@gmail.com>
2021-10-11 22:25:59 -0400

    Added basic navigation bars.

--- ts_frontend/components/session/welcome.jsx
+++ ts_frontend/components/session/welcome.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
 export default () => (
-    <div>
-        <Container>
-            <Row>
-                <Col>
-                    <Form>
-                        <Form.Group>
-                            <Link to="/login">Login</Link>
-                        </Form.Group>
-
-                        <Form.Group>
-                            <Link to="/signup">Create account</Link>
-                        </Form.Group>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-
-    </div>
+    <Container>
+        <Row>
+            <Col>
+            Welcome!
+            </Col>
+        </Row>
+    </Container>
 );
