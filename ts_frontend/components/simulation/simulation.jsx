170e1c102506e0a2a921490aa2f5cd0ce22ac5c5
hannahliu-github <shhan.liu@gmail.com>
2021-11-20 21:35:00 -0500

    Update state when new quote data is received.

--- ts_frontend/components/simulation/simulation.jsx
+++ ts_frontend/components/simulation/simulation.jsx
@@ -16,19 +16,19 @@ class Simulation extends React.Component {
 
         const initial_cash = this.props.simulation.initial_cash;
         const tickers = this.props.simulation.security_set.split('_');
-        const pre_open_points = 1;
+        const pre_open_points = 0;
 
         this.state = {
             account_value: initial_cash,
-            portfolio: { cash: { units: initial_cash, market_value: initial_cash }, 'GOOGL': { units: 10, market_value: 13030.0}, 'AAPL': { units: 2, market_value: 604}},
-            portfolio_tickers: ['AAPL', 'GOOGL'],
+            portfolio: { cash: { units: initial_cash, market_value: initial_cash }, 'GOOGL': { units: 10, market_value: 13030.0}, 'AAPL': { units: 2, market_value: 604}, 'F': { units: 1, market_value: 20 }},
+            portfolio_tickers: ['AAPL', 'F', 'GOOGL'],
             stock_tickers: tickers,
             simulation_id: this.props.simulation.id,
-            simulation_time: this.props.simulation.start_time * 1000,
+            simulation_time: (this.props.simulation.start_time - 1) * 1000,
             // time series
             quote_times: new Array(pre_open_points).fill(0).map((_, i) => (this.props.simulation.start_time - pre_open_points + i) * 1000),
             account_values: new Array(pre_open_points).fill(initial_cash),
-            stock_prices: { 'AAPL': {price: [300, 301, 302], upDownPct: 0.006667}, 'GOOGL': {price: [1305, 1302, 1303], upDownPct: -0.00153} }, // Object.fromEntries(tickers.map(ticker => [ticker, {price: {}, upDownPct: 0}]))
+            stock_prices: Object.fromEntries(tickers.map(ticker => [ticker, {price: {}, upDownPct: 0}])),
             // start/pause/stop states
             simulationIsRunning: false,
             simulationIsStopped: false
@@ -78,7 +78,7 @@ class Simulation extends React.Component {
             $.ajax({
                 url: '/api/tick',
                 method: 'GET',
-                data: { tick: { simulation_id: this.props.simulation.id, timestamp: this.state.simulation_time / 1000 } },
+                data: { tick: { simulation_id: this.props.simulation.id, timestamp: (this.state.simulation_time / 1000) + 1} },
                 complete: () => {
                     // schedule the next request only when the current one is complete
                     setTimeout(this.requestTickUpdates, 1000);
@@ -95,21 +95,57 @@ class Simulation extends React.Component {
     receiveTickUpdate(response) {
         // update simulation time, quotes and portfolio with data from new tick
 
-        const newSimulationTime = this.state.simulation_time + 1000;
+        // handle errors
+        if (response.errors && (response.errors === ["Requested tick after simulation end."])) {
+            console.log('Simulation ended naturally');
+            this.stopSimulation();
+        }
+ 
+        if (!response.tick || !response.quotes) {
+            console.log('Error receiving ticks');
+            if (errors) { console.log(errors); }
+            this.pauseSimulation();
+        }
+
+        // update time
+        const newSimulationTime = response.tick.timestamp * 1000;
+
+        // update quotes
+        let newStockPrices = this.state.stock_prices;
+        for (const t of this.state.stock_tickers) {
+            const newPrice = response.quotes[t]
+            newStockPrices[t].price[newSimulationTime] = newPrice;
+
+            const newUpDownPct = Object.keys(newStockPrices[t].price).length > 0 ? 
+                newPrice / newStockPrices[t].price[this.props.simulation.start_time * 1000]  - 1 : 0;
+            newStockPrices[t].upDownPct = newUpDownPct;
+        }
+
+        // update portfolio market values
+        let newPortfolio = this.state.portfolio;
+        let newAccountValue = this.state.portfolio.cash.market_value;
+        for (const t of this.state.portfolio_tickers) {
+            newPortfolio[t].market_value = newPortfolio[t].units * newStockPrices[t].price[newSimulationTime];
+            newAccountValue += newPortfolio[t].market_value;
+        }
+
         // fake data for testing:
-        const newAccountValue = this.state.account_value + (Math.random() - 0.5) * 10;
+        //const newAccountValue = this.state.account_value + (Math.random() - 0.5) * 10;
 
         this.setState({
             simulation_time: newSimulationTime,
             account_value: newAccountValue,
             quote_times: this.state.quote_times.concat(newSimulationTime),
-            account_values: this.state.account_values.concat(newAccountValue)
+            account_values: this.state.account_values.concat(newAccountValue),
+            stock_prices: newStockPrices
         });
     }
 
 
     formatDollarAmount(num) {
         // Return num with comma separators and 2 decimal places.
+        if (!num) {return ''}
+
         let numStr = Number(num.toFixed(2)).toLocaleString();
 
         // ensure 2 decimal places
@@ -139,6 +175,22 @@ class Simulation extends React.Component {
 
 
     componentDidMount() {
+
+        if (this.state.simulation_time < this.props.simulation.start_time * 1000) {
+            // send request for first tick, allow some time for database to load
+            setTimeout(() =>
+            {
+                $.ajax({
+                    url: '/api/tick',
+                    method: 'GET',
+                    data: { tick: { simulation_id: this.props.simulation.id, timestamp: (this.state.simulation_time / 1000) + 1 } },
+                    success: (response) => {
+                        // receive update in callback
+                        this.receiveTickUpdate(response);
+                    }
+                })
+            }, 500);
+        }
 
         // poftfolio chart
 
@@ -221,7 +273,7 @@ class Simulation extends React.Component {
                 <td style={{ textAlign: 'right', color: this.state.stock_prices[t].upDownPct >= 0 ? upColor : downColor }}>
                     {(100 * this.state.stock_prices[t].upDownPct).toFixed(2)}%</td>
                 <td style={{ textAlign: 'right', color: this.state.stock_prices[t].upDownPct >= 0 ? upColor : downColor }}>
-                    {this.formatDollarAmount(this.state.stock_prices[t].price[2])}</td>
+                    {this.formatDollarAmount(this.state.stock_prices[t].price[this.state.simulation_time])}</td>
                 <td style={{ textAlign: 'center' }}>
                     {this.state.portfolio[t].units}</td>
                 <td style={{ textAlign: 'right', color: '#ff6600'}}>
@@ -258,7 +310,7 @@ class Simulation extends React.Component {
                     Simulation date: {new Date(this.state.simulation_time).toLocaleString('en-US', { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' })}
                 </Row>
                 <Row className="time-row">
-                    Time: {new Date(this.state.simulation_time).toLocaleTimeString()}
+                            Time: {new Date(Math.max(this.state.simulation_time, this.props.simulation.start_time * 1000)).toLocaleTimeString()}
                 </Row>
                 </Col>
                 </Row>
